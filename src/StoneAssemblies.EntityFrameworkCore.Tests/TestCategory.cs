424aeb187dda002ec023b38a42879869d2773d99
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2023-07-30 10:15:48 -0400

    Adds a test project

---
+++ src/StoneAssemblies.EntityFrameworkCore.Tests/TestCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace StoneAssemblies.EntityFrameworkCore;
+
+public static class TestCategory
+{
+    public const string Integration = nameof(Integration);
+
+    public const string Unit = nameof(Unit);
+}
---
+++ src/StoneAssemblies.EntityFrameworkCore.Tests/Traits.cs
@@ -0,0 +1,6 @@
+﻿namespace StoneAssemblies.EntityFrameworkCore;
+
+public static class Traits
+{
+    public const string Category = nameof(Category);
+}
